2e59cd502056d32b131bb201c34439715e740290
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-15 04:40:14 -0300

    Criando camada de servico e enviando valores para o servidor rabbitMQ
    Criando camada de preparação dos valores para enviar para o servidor do rabbitMQ.

--- src/main/java/com/project/estoque/controller/PrecoController.java
+++ src/main/java/com/project/estoque/controller/PrecoController.java
@@ -1,5 +1,6 @@
 package com.project.estoque.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,14 +8,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.estoque.constant.RabbitmqConstantes;
 import com.project.estoque.dto.PrecoDto;
+import com.project.estoque.service.RabbitmqService;
 
 @RestController
 @RequestMapping("/api/v1/preco/")
 public class PrecoController {
+	
+	@Autowired
+	RabbitmqService service;
 
 	@PutMapping
 	private ResponseEntity<PrecoDto> alteraEstoque(@RequestBody PrecoDto precoDto) {
+		this.service.sendMensage(RabbitmqConstantes.FILA_PREÇO, precoDto);
 		return new ResponseEntity<PrecoDto>(HttpStatus.OK);
 	}
 	
