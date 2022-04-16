f10b7f808a121e432d897c411b1e4717f1e43cf8
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-15 23:49:11 -0300

    injetendo service no controller estoque
    injetendo dependencia de service no controller

--- src/main/java/com/project/estoque/controller/EstoqueController.java
+++ src/main/java/com/project/estoque/controller/EstoqueController.java
@@ -1,5 +1,6 @@
 package com.project.estoque.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,14 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.estoque.constant.RabbitmqConstantes;
 import com.project.estoque.dto.EstoqueDto;
+import com.project.estoque.service.RabbitmqService;
 
 @RestController
 @RequestMapping("/api/v1/estoque/")
 public class EstoqueController {
-
+	
+	@Autowired
+	RabbitmqService service;
+	
 	@PutMapping
 	private ResponseEntity<EstoqueDto> alteraEstoque(@RequestBody EstoqueDto  estoqueDto) {
+		this.service.sendMensage(RabbitmqConstantes.FILA_ESTOQUE, estoqueDto);
+		System.out.println(estoqueDto.getQuantidade());
 		return new ResponseEntity<EstoqueDto>(HttpStatus.OK);
 	}
 	
